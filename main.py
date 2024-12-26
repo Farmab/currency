7bfeaf47911c5ac8bf6ed26635eecc6b8859fd45
Farmab <49933824+Farmab@users.noreply.github.com>
2024-12-26 18:36:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from visualization import setup_ui, display_conversion_section
 from exchange_rate import get_exchange_rates
+from visualization import setup_ui, display_conversion_section
 
 def main():
-    # Setup the UI (page title, layout, and styles)
     setup_ui()
-
-    # Fetch the exchange rates
     exchange_rates = get_exchange_rates()
-
     if exchange_rates:
-        # Display the conversion section
         display_conversion_section(exchange_rates)
     else:
-        st.error("Unable to fetch the exchange rates. Please try again later.")
+        st.error("Unable to fetch exchange rates.")
 
 if __name__ == "__main__":
     main()
